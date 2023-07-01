76c83a9ba93892a575ed85100f7e8e510c020330
Min Hyuk Choi <minhyukchoi@Min-Hyuks-MacBook-Pro.local>
2023-07-01 01:32:35 -0700

    AT-3 Created an header with the text TODO List and imported Amatic SC from Google Fonts

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import QuotePage from "./pages/QuotePage";
 import Quote from "./component/quote";
+import HeaderPage from "./pages/HeaderPage";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/">
+          <HeaderPage />
           <QuotePage />
         </Route>
       </Router>
