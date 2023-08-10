6da3d7af1cd889a448b05b3886547d41cc373f0b
walshcommaryan <rywalsh@uci.edu>
2023-08-10 10:30:30 -0700

    AT-21 scroll fix

--- client/src/component/ToDoList.js
+++ client/src/component/ToDoList.js
@@ -11,13 +11,15 @@ const ToDoList = () => {
   // Function to add a new task to the todo list
   const addCard = () => {
     setEmptyCards((currCards) => [...currCards, {id: currCards.length}]); //"..." copies all the values from the old array
-    let todoList = document.querySelector('#todo-list');
-    todoList.scrollTop = todoList.scrollHeight;
+    setTimeout(() => {
+      let todoList = document.querySelector('#todo-list');
+      todoList.scrollTop = todoList.scrollHeight;
+    }, 0);
   };
 
   return (
     <div className="flex flex-col">
-      <ul id="todo-list" className="border-4 border-yellow-500 max-h-80 overflow-y-auto list-none">
+      <ul id="todo-list" className="border-4 border-yellow-500 max-h-80 overflow-y-auto list-none" style={{ scrollBehavior: 'smooth' }}>
         {emptyCards.map((card) => ( //map is what renders the data into the screen
           <li key={card.id }>
             <EmptyCard/>
